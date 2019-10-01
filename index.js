b4adb2a606f519c43b4764dc904f9bdb29e552c1
Julli-Cumming <jullicumming@gmail.com>
2019-10-01 09:12:52 -0300

    Commit

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const express = require("express");
 
 const server = express();
 
-// localhost:3000/teste
-server.get("/teste", (req, res) => {
-  return res.send("Hello World! Bem vindo ao Node.js");
+server.use(express.json());
+
+// localhost:3000/users/123
+//return res.json({ message: `Hello ${nome}` });
+//return res.send("Hello World! Bem vindo ao Node.js");
+// return res.json({ messagem: "Hello World! Bem vindo ao Node.js" });
+
+//crud - Create, Read, Update e Delete
+
+const users = ["Fabio", "Paulo", "Daniel"];
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  return res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put('/users/: index', (req, res) => {
+const { index } = req.params;
+const { name } = req.body;
+
+users{index} = name;
+return res.json(users);
+
 });
 
 server.listen(3000);
+
+// Query params = ? teste=1
+// Route params = /users/1
+// Request body = { "name": "Julli", "email": "teste@gmail.com"}
